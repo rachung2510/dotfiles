9d70f4a23ae0c7ec36137b1c665e1304bb446aa5
rachung2510 <rachung2510@gmail.com>
2022-07-30 02:41:14 +0800

    add consumption to polybar battery popup

--- polybar/colorblocks/scripts/battery.sh
+++ polybar/colorblocks/scripts/battery.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 dir="~/.config/polybar/colorblocks/scripts/rofi"
-var="$(acpi -b)"
-msg="${var##*,}"
+remaining_time="$(acpi -b)"
+volt=$(cat /sys/class/power_supply/BAT1/voltage_now)
+curr=$(cat /sys/class/power_supply/BAT1/current_now)
+watt=$(echo "scale=2; ${volt}*${curr}/1000000/1000000" | bc)
+msg="${remaining_time##*, }
+${watt} W discharging"
 rofi -theme "$dir/message.rasi" -e "$msg"
