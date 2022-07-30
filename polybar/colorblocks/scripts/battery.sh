44d279db2962aca837a801850c8b5eba261ce2ef
rachung2510 <rachung2510@gmail.com>
2022-07-30 20:58:03 +0800

    update polybar battery popup for charging

--- polybar/colorblocks/scripts/battery.sh
+++ polybar/colorblocks/scripts/battery.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 dir="~/.config/polybar/colorblocks/scripts/rofi"
-remaining_time="$(acpi -b)"
-volt=$(cat /sys/class/power_supply/BAT1/voltage_now)
-curr=$(cat /sys/class/power_supply/BAT1/current_now)
-watt=$(echo "scale=2; ${volt}*${curr}/1000000/1000000" | bc)
-msg="${remaining_time##*, }
-${watt} W discharging"
+status="$(cat /sys/class/power_supply/BAT1/status)"
+
+time_msg="$(acpi -b)"
+duration="${time_msg##*, }"
+
+if [[ $status = "Charging" ]]; then
+	msg="Time until charged: ${duration% until charged*}"
+else
+	volt=$(cat /sys/class/power_supply/BAT1/voltage_now)
+	curr=$(cat /sys/class/power_supply/BAT1/current_now)
+	watt=$(echo "scale=2; ${volt}*${curr}/1000000/1000000" | bc)
+	msg="Time remaining: ${duration% remaining*}
+Consumption: ${watt} W"
+fi
+
 rofi -theme "$dir/message.rasi" -e "$msg"
