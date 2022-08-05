ae52e6355303f9909b96a237514662c97d3f95a5
rachung2510 <rachung2510@gmail.com>
2022-08-06 00:25:10 +0800

    add esc/return to escape eww sidebar

--- i3/scripts/eww.sh
+++ i3/scripts/eww.sh
@@ -2,6 +2,8 @@
 
 windows="$(eww windows) eol"
 open_window="$(echo $windows | grep -oP '(?<=\*).*?(?= )')"
+ESC=9
+RETURN=36
 
 #echo $windows
 #echo $open_window
@@ -15,7 +17,16 @@ if [[ $open_window = "" ]]; then
 		gnome-terminal --hide-menubar --window-with-profile=dc1 --role=dc1 -- cava -p ~/.config/cava/config &
 	fi
 	sleep 0.15 && eww open $1
+	xinput test-xi2 --root 3 | grep -A2 --line-buffered RawKeyRelease | while read -r line; do
+	    if [[ $line == *"detail"* ]]; then
+	        key=$( echo $line | sed "s/[^0-9]*//g")
+			if [[ $key = $ESC ]] || [[ $key = $RETURN ]]; then
+				eww close-all && pkill cava
+				exit 0
+			fi
+		fi
+	done
 else
 	eww close-all && pkill cava
+	killall eww.sh
 fi
-
