b380796f747cb840e73af6887ee984eeb0ff9ff3
rachung2510 <rachung2510@gmail.com>
2022-08-17 12:56:17 +0800

    exit when instance already running

--- i3/scripts/onedrive.sh
+++ i3/scripts/onedrive.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+pid=$(pgrep onedrive.sh)
+if [[ $(echo "$pid" | awk -F '[ ]' '{print (NF?NF-1:0)}') != "0" ]]; then
+	exit 0
+fi
+
 while true; do
 	onedrive --synchronize
 	onedrive --synchronize --confdir=~/.config/onedriveimperial
