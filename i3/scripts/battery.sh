292cd7a27e5f8366e356a3a03937aab85dd3c9c8
rachung2510 <rachung2510@gmail.com>
2022-08-17 21:21:37 +0800

    upload font

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -3,6 +3,18 @@
 LOW_THRESH=15
 HIGH_THRESH=95
 
+high() {
+	notify-send -u low -i ~/.config/dunst/img/battery-full.png -h "string:x-dunst-stack-tag:batt-full" "Battery is fully charged" "Please disconnect your charger"
+}
+
+low() {
+	duration=$(acpi -b | grep -oP '(?<=%, ).*(?= remaining)')
+	notify-send -u critical -i ~/.config/dunst/img/battery-critical.png -h "string:x-dunst-stack-tag:batt-crit" "Your battery is running low" "${duration%:*} remaining"
+}
+
+[[ $1 = "--high" ]] && high && exit 0
+[[ $1 = "--low" ]] && low && exit 0
+
 pid=$(pgrep battery.sh)
 if [[ $(echo "$pid" | awk -F '[ ]' '{print (NF?NF-1:0)}') != "0" ]]; then
 	exit 0
@@ -14,14 +26,9 @@ do
 	POWER=$(acpi -b | grep "Battery 0" | grep -o '[0-9]\+%' | tr -d '%')
 
 	if [[ $STATUS = "Charging" ]]; then
-		if [[ $POWER -ge $HIGH_THRESH ]]; then
-			dunstify -u low -i "~/.config/dunst/img/battery-full.png" -h "string:x-dunst-stack-tag:batt-full" "Battery is fully charged" "Please disconnect your charger"
-		fi
+		if [[ $POWER -ge $HIGH_THRESH ]]; then high; fi
 	else
-		if [[ $POWER -le $LOW_THRESH ]]; then
-			duration=$(acpi -b | grep -oP '(?<=%, ).*(?= remaining)')
-			dunstify -u critical -i "~/.config/dunst/img/battery-critical.png" -h "string:x-dunst-stack-tag:batt-crit" "Your battery is running low" "${duration%:*} remaining"
-		fi
+		if [[ $POWER -le $LOW_THRESH ]]; then low; fi
 	fi
 sleep 60
 #echo "run"
