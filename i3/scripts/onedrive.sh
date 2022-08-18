928512fee71bc99540c194d03077e0b72a5e0b3a
rachung2510 <rachung2510@gmail.com>
2022-08-18 17:39:57 +0800

    add notif when wifi is disconnected

--- i3/scripts/onedrive.sh
+++ i3/scripts/onedrive.sh
@@ -6,6 +6,18 @@ if [[ $(echo "$pid" | awk -F '[ ]' '{print (NF?NF-1:0)}') != "0" ]]; then
 fi
 
 while true; do
+	#check wifi
+	if [[ $(iwgetid -r) = "" ]]; then
+		choice=$(dunstify --action="kill,Kill process" "OneDrive sync failed" "WiFi not connected. Consider killing the OneDrive sync")
+		case "$choice" in
+			"kill")
+				notify-send "OneDrive sync stopped"
+				exit 0
+				;;
+		esac
+	fi
+
+	echo "pass"
 	onedrive --synchronize
 	onedrive --synchronize --confdir=~/.config/onedriveimperial
 	sleep 300
