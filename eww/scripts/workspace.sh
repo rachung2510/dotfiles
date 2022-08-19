f20d2629452d3045123bb3ba180d23fff5ddb942
rachung2510 <rachung2510@gmail.com>
2022-08-20 04:48:19 +0800

    replace jq with grep & awk for faster parsing

--- eww/scripts/workspace.sh
+++ eww/scripts/workspace.sh
@@ -2,20 +2,22 @@
 
 run() {
 	ws_info=$(i3-msg -t get_workspaces)
-	LITERAL="(box :class 'bar-widget workspaces' :orientation 'v' :space-evenly 'falsie' :spacing '3' :width bar-width"
-	len=$(echo $ws_info | jq 'length')
-	NUMS=$(echo $ws_info | jq '.[] | .num')
-	NAMES=$(echo $ws_info | jq '.[] | .name')
-	URGENTS=$(echo $ws_info | jq '.[] | .urgent')
-	FOCUSES=$(echo $ws_info | jq '.[] | .focused')
+	LITERAL="(box :class 'bar-widget workspaces' :orientation 'v' :space-evenly 'false' :spacing '3' :width bar-width"
+	NUMS=$(echo $ws_info | grep -oP '"num":\s*\K[^\s,]*(?=\s*,)')
+	# NAMES=$(echo $ws_info | grep -oP '"name":"*\K[^,]*(?=",)')
+	URGENTS=$(echo $ws_info | grep -oP '"urgent":\s*\K[^\s,]*(?=\s*})')
+	FOCUSES=$(echo $ws_info | grep -oP '"focused":\s*\K[^\s,]*(?=\s*,)')
+	len=$(echo $NUMS | wc -w)
 	for i in $(seq 1 $len); do
-		NUM=$(echo $NUMS | awk -F' ' '{print $'$i'}')
-		NAME=$(echo ${NAMES//\"/} | awk -F' ' '{print $'$(( i*2 ))'}')
-		URGENT=$(echo $URGENTS | awk -F' ' '{print $'$i'}')
-		FOCUSED=$(echo $FOCUSES | awk -F' ' '{print $'$i'}')
-		MODI=""
-		if [[ $URGENT = true ]]; then MODI="urgent"; fi
-		if [[ $FOCUSED = true ]]; then MODI="focused"; fi
+		NUM=$(echo $NUMS | awk '{print $'$i'}')
+		NAME=$(echo $ws_info | grep -oP '"name":"[0-9]\s*\K[^,]*(?=",)' | awk 'FNR == '$i)
+		URGENT=$(echo $URGENTS | awk '{print $'$i'}')
+		FOCUSED=$(echo $FOCUSES | awk '{print $'$i'}')
+		[[ $URGENT = true ]] && MODI="urgent" || MODI=""
+		[[ $FOCUSED = true ]] && MODI="focused"
+		# MODI=""
+		# if [[ $URGENT = true ]]; then MODI="urgent"; fi
+		# if [[ $FOCUSED = true ]]; then MODI="focused"; fi
 		LITERAL="$LITERAL (ws :num $NUM :name '$NAME' :modi '$MODI')"
 	done
 
