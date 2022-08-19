b17201507d05f974bb000d323915ab82813fe65a
rachung2510 <rachung2510@gmail.com>
2022-08-19 13:35:45 +0800

    add desktop screenshot

--- i3/scripts/screenshot.sh
+++ i3/scripts/screenshot.sh
@@ -4,8 +4,12 @@ dir="$HOME/Pictures/Screenshots"
 [[ ! -d $dir ]] && mkdir -p $dir
 path="$dir/$(date '+%Y-%m-%d %H:%M:%S')_screenshot.png"
 
-maim -s "$path"
-xclip -selection clipboard -t image/png "$path"
+if [[ $1 = 'desktop' ]]; then
+	maim "$path"
+else
+	maim -s "$path"
+	xclip -selection clipboard -t image/png "$path"
+fi
 
 if [[ -f $path ]]; then
 	notify-send -a maim -u normal -i "$path" "Snip saved to clipboard" "Screenshot saved to Pictures."
