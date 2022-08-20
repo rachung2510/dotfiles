0674b95d405898d87e0011dc8754866151aea1dd
rachung2510 <rachung2510@gmail.com>
2022-08-21 01:44:17 +0800

    fix cava position and delay

--- i3/scripts/eww.sh
+++ i3/scripts/eww.sh
@@ -24,7 +24,7 @@ elif [[ $1 = "sidebar" ]]; then
 	eww close bar
 	theme=$(~/.config/i3/scripts/theme.sh -p)
 	gnome-terminal --hide-menubar --window-with-profile=cava-$theme --role=cava -- cava -p ~/.config/cava/config &
-	sleep 0.2 && eww open sidebar
+	sleep 0.24 && eww open sidebar
 	xinput test-xi2 --root 3 | grep -A2 --line-buffered RawKeyRelease | while read -r line; do
 	    if [[ $line == *"detail"* ]]; then
 	        key=$( echo $line | sed "s/[^0-9]*//g")
