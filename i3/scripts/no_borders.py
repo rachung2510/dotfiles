ca7edf1be20a4ee43aebe4acdb99567fbcca56b5
rachung2510 <rachung2510@gmail.com>
2023-05-23 19:46:29 +0100

    make no_borders exec_always and include exclusion window_classes

--- i3/scripts/no_borders.py
+++ i3/scripts/no_borders.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from i3ipc import Event, Connection
 
+EXCLUDE = ["stalonetray", "Nextcloud"]
+
 def on_focus(i3, e):
 	workspace = i3.get_tree().find_focused().workspace()
 	for leaf in workspace.leaves():
-		# print(f"{leaf.window_title}: {leaf.focused}")
-		if leaf.focused:
+		print(f"{leaf.window_class}, {leaf.window_title}: {leaf.focused}")
+		if leaf.focused and not leaf.window_class in EXCLUDE:
 			leaf.command("border pixel 3")
 		else:
 			leaf.command("border none")
