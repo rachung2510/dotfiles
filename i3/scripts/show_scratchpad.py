9a55dd51e730a95f55729f698ab157d447d92933
rachung2510 <rachung2510@gmail.com>
2023-05-24 02:15:37 +0100

    add rofi theme for scratchpad

--- i3/scripts/show_scratchpad.py
+++ i3/scripts/show_scratchpad.py
@@ -3,7 +3,7 @@ from i3ipc import Connection
 i3 = Connection()
 lst = []
 for leaf in i3.get_tree().scratchpad().leaves():
-	s = f'{leaf.window} {leaf.window_class} {leaf.window_title}'
+	s = f'{leaf.window} {leaf.window_class}: {leaf.window_title}'
 	lst.append(s)
 
 print("\n".join(lst))
