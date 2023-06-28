6b4ec2da44c3f069d98d2c6f024070c97edb17b9
rachung2510 <rachung2510@gmail.com>
2023-06-29 00:25:41 +0100

    add rule for Mailspring, rename icons folder & add getinfo script

---
+++ dunst/getinfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "1: $1" > ~/.config/dunst/info
+echo "2: $2" >> ~/.config/dunst/info
+echo "3: $3" >> ~/.config/dunst/info
+echo "4: $4" >> ~/.config/dunst/info
+echo "5: $5" >> ~/.config/dunst/info
+echo "6: $6" >> ~/.config/dunst/info
